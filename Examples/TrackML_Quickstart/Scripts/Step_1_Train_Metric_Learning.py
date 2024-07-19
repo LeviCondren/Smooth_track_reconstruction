eee79600060fbf4aeefbcb1c0ede8f7c1238f8e8
Levi <85909265+LeviCondren@users.noreply.github.com>
2024-07-19 15:05:51 -0700

    Changed default Trainer accelerator None ->'auto'

--- Examples/TrackML_Quickstart/Scripts/Step_1_Train_Metric_Learning.py
+++ Examples/TrackML_Quickstart/Scripts/Step_1_Train_Metric_Learning.py
@@ -46,7 +46,7 @@ def train(config_file="pipeline_config.yaml"):
     logger = CSVLogger(save_directory, name=common_configs["experiment_name"])
 
     trainer = Trainer(
-        accelerator='gpu' if torch.cuda.is_available() else None,
+        accelerator='gpu' if torch.cuda.is_available() else 'auto',
         devices=common_configs["gpus"],
         max_epochs=metric_learning_configs["max_epochs"],
         logger=logger
